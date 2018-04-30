5b50cf46bfa25ecab0779ef388cf136ca2fcf24c
Andrew Grimberg <tykeal@bardicgrove.org>
2018-04-29 18:15:59 -0700

    Make hiera lookup clamp to bool types
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/auditd.pp
+++ manifests/auditd.pp
@@ -3,7 +3,10 @@ class profile::auditd {
   include ::auditd
 
   if lookup('auditd::enable_syslog',
-    { 'default_value' => false }) {
+    { 
+      'default_value' => false,
+      'value_type'    => Boolean,
+    }) {
     include ::auditd::audisp::syslog
   }
 }
