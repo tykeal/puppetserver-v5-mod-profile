1f6f95803659c211cb077a3a72197f0378abbdb4
Andrew Grimberg <tykeal@bardicgrove.org>
2018-05-06 11:12:20 -0700

    Add puppetdb firewall
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/puppetdb/postgresql.pp
+++ manifests/puppetdb/postgresql.pp
@@ -1,4 +1,12 @@
 # puppetdb postgresql setup
 class profile::puppetdb::postgresql {
   include ::puppetdb::database::postgresql
+
+  profile::firewall::rule { 'Postgres traffic from all':
+    priority => '010',
+    proto    => 'tcp',
+    dport    => '5432',
+    state    => ['NEW'],
+    action   => 'accept',
+  }
 }
--- manifests/puppetdb/server.pp
+++ manifests/puppetdb/server.pp
@@ -1,4 +1,12 @@
 # Configure the puppetdb server
 class profile::puppetdb::server {
   include ::puppetdb::server
+
+  profile::firewall::rule { 'Puppetdb traffic from all':
+    priority => '010',
+    proto    => 'tcp',
+    dport    => '8081',
+    state    => ['NEW'],
+    action   => 'accept',
+  }
 }
