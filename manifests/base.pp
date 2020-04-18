4ecb2fae0ff03fd251fd80cc7c1f0978040ca88b
Andrew Grimberg <tykeal@bardicgrove.org>
2020-04-18 14:55:19 -0700

    Turn on hosts module again
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/base.pp
+++ manifests/base.pp
@@ -10,7 +10,7 @@ class profile::base {
   include ::profile::haveged
   # temporarily remove hosts management as there seems to be bug
   # when puppetdb isn't working yet
-  # include ::profile::hosts
+  include ::profile::hosts
   # ntp doesn't exist on EL8, I need to investigate a chrony module
   # include ::profile::ntp
   include ::profile::pam
