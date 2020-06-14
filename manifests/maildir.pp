cc1f2937acc597ac8a41f4dd4659abf1eefc6eed
Andrew Grimberg <tykeal@bardicgrove.org>
2020-06-14 13:13:30 -0700

    Add Maildir to /etc/skel
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

---
+++ manifests/maildir.pp
@@ -0,0 +1,19 @@
+# profile::maildir
+class profile::maildir {
+  # we host our user mail in Maildirs
+  #
+  # should we create a new user we want them to get that setup correctly
+  # so we need to make sure that the Maildir structure exists in /etc/skel
+  file {
+    [
+      '/etc/skel/Maildir',
+      '/etc/skel/Maildir/cur',
+      '/etc/skel/Maildir/new',
+      '/etc/skel/Maildir/tmp',
+    ]:
+    ensure => directory,
+    group  => 'root',
+    mode   => '0700',
+    owner  => 'root',
+  }
+}
