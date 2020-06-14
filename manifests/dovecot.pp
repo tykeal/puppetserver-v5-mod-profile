1567beb2d76b85f2afb90cd297a96af5e9c34ad5
Andrew Grimberg <tykeal@bardicgrove.org>
2020-06-14 07:08:04 -0700

    Load certificate define values from hiera
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/dovecot.pp
+++ manifests/dovecot.pp
@@ -16,8 +16,38 @@ class profile::dovecot {
     $cert_host = $dovecot_config['hostname']
   }
 
-  # The rest of the values should properly load from hiera
-  ::acme::certificate { $cert_host : }
+  # load the acme certificate values as we're going to call a define and those
+  # don't auto-lookups
+  $acme_use_account = lookup(
+    'acme::certificate::use_account',
+    {
+      # no default value as it's required to be set
+      value_type => String,
+    }
+  )
+
+  $acme_use_profile = lookup(
+    'acme::certificate::use_profile',
+    {
+      # no default value as it's required to be set
+      value_type => String,
+    }
+  )
+
+  $acme_le_ca = lookup(
+    'acme::certificate::letsencrypt_ca',
+    {
+      # This is allowed to be undef (defaults to production)
+      default_value => undef,
+      value_type    => String,
+    }
+  )
+
+  ::acme::certificate { $cert_host :
+    use_account    => $acme_use_account,
+    use_profile    => $acme_use_profile,
+    letsencrypt_ca => $acme_le_ca,
+  }
 
   # pops/imaps
   ::profile::firewall::rule { 'Enable POPS/IMAPS':
