a177315786bed901b88ca7ba8167ae46b3c165fb
Andrew Grimberg <tykeal@bardicgrove.org>
2020-06-14 06:58:16 -0700

    Setup acquiring certs for dovecot
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/dovecot.pp
+++ manifests/dovecot.pp
@@ -2,6 +2,23 @@
 class profile::dovecot {
   include ::dovecot
 
+  # load the dovcecot config to determine the cert name we should be acquiring
+  $dovecot_config = lookup(
+    'dovecot::config',
+    {
+      default_value => {},
+      value_type    => Hash,
+    }
+  )
+
+  if has_key($dovecot_config, 'hostname')
+  {
+    $cert_host = $dovecot_config['hostname']
+  }
+
+  # The rest of the values should properly load from hiera
+  ::acme::certificate { $cert_host : }
+
   # pops/imaps
   ::profile::firewall::rule { 'Enable POPS/IMAPS':
     priority => '645',
