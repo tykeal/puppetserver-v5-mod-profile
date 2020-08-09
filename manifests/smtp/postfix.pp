ab37f320b61cf99721104d5e5b330604681d9390
Andrew Grimberg <tykeal@bardicgrove.org>
2020-08-09 09:05:21 -0700

    Add acme cert configuration
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/smtp/postfix.pp
+++ manifests/smtp/postfix.pp
@@ -80,4 +80,43 @@ class profile::smtp::postfix {
       action   => 'accept',
     }
   }
+
+  if has_key($postfix_configs, 'myhostname')
+  {
+    $cert_host = $postfix_configs['myhostname']
+  }
+
+  # load the acme certificate values as we're going to call a define and those
+  # don't do auto-lookups
+  $acme_use_account = lookup(
+    'acme::certificate::use_account',
+    {
+      # no default value as it's required to be set
+      value_type => String,
+    }
+  )
+
+  $acme_use_profile = lookup(
+    'acme::certificate::use_profile',
+    {
+      # no default value as it's required to be set
+      value_type => String,
+    }
+  )
+
+  $acme_le_ca = lookup(
+    'acme::certificate::letsencrypt_ca',
+    {
+      # This is allowed to be undef (defaults to production)
+      default_value => undef,
+      value_type    => String,
+    }
+  )
+
+  ::acme::certificate { $cert_host :
+    use_account    => $acme_use_account,
+    use_profile    => $acme_use_profile,
+    letsencrypt_ca => $acme_le_ca,
+  }
+
 }
