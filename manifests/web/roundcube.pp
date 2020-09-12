71e39113f8b4f352d782734673eaf8cfdd11f965
Andrew Grimberg <tykeal@bardicgrove.org>
2020-09-12 13:39:20 -0700

    Install composer
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/web/roundcube.pp
+++ manifests/web/roundcube.pp
@@ -2,6 +2,14 @@
 class profile::web::roundcube {
   include ::roundcube
 
+  # The module only understands debian, so we need to do a little extra work
+  ensure_packages (
+    [
+      'composer',
+    ],
+    { ensure => installed }
+  ) -> Class['roundcube']
+
   # Deal with creating the DB (we're only going to do mysql)
   $db_name = lookup(
     'roundcube::db_name',
