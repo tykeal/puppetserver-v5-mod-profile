6f00a5d7560507100fcbad7ea15e475cd024bb76
Andrew Grimberg <tykeal@bardicgrove.org>
2021-01-04 12:56:28 -0800

    remove unneeded test profile classes
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/testit.pp
+++
@@ -1,8 +0,0 @@
-# Vim profile
-class profile::testit {
-  file { '/root/testit':
-    ensure => present,
-    owner  => 'root',
-    group  => 'tykeal',
-  }
-}
--- manifests/testitagain.pp
+++
@@ -1,8 +0,0 @@
-# Vim profile
-class profile::testitagain {
-  file { '/root/testitagain':
-    ensure => present,
-    owner  => 'root',
-    group  => 'tykeal',
-  }
-}
