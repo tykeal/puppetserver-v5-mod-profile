022813a463ff915070f8bd60f69d9908c15cc7d8
Andrew Grimberg <tykeal@bardicgrove.org>
2021-03-10 11:00:22 -0800

    Modify how function is designed
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- functions/monitorinfo.pp
+++ functions/monitorinfo.pp
@@ -4,34 +4,34 @@ function profile::monitorinfo() >> Struct[{
   nagios_plugin_dir    => Stdlib::AbsolutePath,
   defaultserviceconfig => Hash,
 }] {
-  $monitor_info = {}
   include ::nagios::params
-  $monitor_info['nagios_plugin_dir'] = lookup(
-    'nagios_plugin_dir',
-    {
-      default_value => '/usr/lib64/nagios/plugins',
-      value_type    => Stdlib::AbsolutePath,
-    }
-  )
-  $monitor_info['defaultserviceconfig'] = lookup(
-    'nagios::client::defaultserviceconfig',
-    {
-      default_value => $::nagios::params::defaultserviceconfig,
-      value_type    => Hash,
-    }
-  )
-
-  $monitor_info['enable'] = (lookup(
-    'production_ready',
-    {
-      default_value => false,
-      value_type    => Boolean,
-    }
-  ) or (lookup(
-    'lfcorehost',
-    {
-      default_value => false,
-      value_type    => Boolean,
-    }
-  )))
+  $monitor_info = {
+    enable               => (lookup(
+      'production_ready',
+      {
+        default_value => false,
+        value_type    => Boolean,
+      }
+    ) or (lookup(
+      'lfcorehost',
+      {
+        default_value => false,
+        value_type    => Boolean,
+      }
+    ))),
+    defaultserviceconfig => lookup(
+      'nagios::client::defaultserviceconfig',
+      {
+        default_value => $::nagios::params::defaultserviceconfig,
+        value_type    => Hash,
+      }
+    ),
+    nagios_plugin_dir    => lookup(
+      'nagios_plugin_dir',
+      {
+        default_value => '/usr/lib64/nagios/plugins',
+        value_type    => Stdlib::AbsolutePath,
+      }
+    ),
+  }
 }
